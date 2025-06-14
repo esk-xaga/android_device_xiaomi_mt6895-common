bc1c4170750c93b70a70a2349d1ae022ba58deb6
ChrisCatto <chriscatto@proton.me>
2025-06-14 07:48:51 +0000

    mt6895-common: Refactor keymint service patching
    * There is an issue with keymint V4 NDK in our device because of some
      changes to keystore2 related to setAdditionalAttestationInfo and
      after Android 16 release and it seems like we cannot boot by keep
      using it. Downgrading keymint NDK to V1 (as it was meant to be
      originally) and RKP NDK to V1 allows us to boot again.
    
    * However, in order to keep checkelf enabled for keymint service, we
      need to bring some keymint related blobs from stock and link them
      against our keymint service. These blobs from stock also had to be
      patched to pass checkelf. In addition, this does not look like the
      best solution (there is some logspam about RKP being disabled) but it
      can be used temporarily until a better fix is found.
    
    Change-Id: I7e75ddb7706b1c8f97baee129295d0dbd35d142f
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -50,10 +50,21 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     'vendor/bin/hw/android.hardware.security.keymint@1.0-service.beanpod': blob_fixup()
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V4-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
-        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_keymint_utils-v31.so')
+        .replace_needed('libkeymint.so', 'libkeymint-v31.so')
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
+    'vendor/lib64/lib_android_keymaster_keymint_utils-v31.so': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so'),
+    'vendor/lib64/libkeymint-v31.so': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('lib_android_keymaster_keymint_utils.so', 'lib_android_keymaster_keymint_utils-v31.so')
+        .replace_needed('libkeymaster_portable.so', 'libkeymaster_portable-v31.so')
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     'vendor/lib64/hw/mt6895/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so')
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
